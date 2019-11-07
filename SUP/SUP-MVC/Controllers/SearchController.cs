a5cb9c1895fd4df099c793cbe4ca7cbacda85216
dnguye14 <dnguye14@uwsuper.edu>
2019-11-06 21:22:29 -0600

    Finish the excel file download feature

--- SUP/SUP-MVC/Controllers/SearchController.cs
+++ SUP/SUP-MVC/Controllers/SearchController.cs
@@ -9,6 +9,7 @@ using SUP_Library;
 using SUP_MVC.Models.Search;
 using System.Net;
 using SUP_Library.DBComponent;
+using System.IO;
 
 namespace SUP_MVC.Controllers
 {
@@ -154,7 +155,9 @@ namespace SUP_MVC.Controllers
                     if (currentClient != null)
                         clientArray.Add(currentClient);
                 }
-                ExportFile.CreateExcelFile2(clientArray);
+
+                string fileName;
+                ExportFile.CreateExcelFile2(clientArray, out fileName);
                 /*
                 using (WebClient client = new WebClient())
                 {
@@ -166,8 +169,9 @@ namespace SUP_MVC.Controllers
 
                 //TODO: return value should describe whether or not the process worked to the client.
                 //  The below line is meaningless until then.
-                var json = JsonConvert.SerializeObject("");
+                var json = JsonConvert.SerializeObject(fileName);
                 return json;
+                
             }
             catch (Exception e)
             {
@@ -180,5 +184,16 @@ namespace SUP_MVC.Controllers
 
 			return View();
 		}
+
+        [HttpGet]
+        public ActionResult Download(string fileName)
+        {
+            //Get the temp folder and file path in server
+            string savePath = @"C:\Users\Public\Documents\SUPExport";
+            string fullPath = Path.Combine(savePath, fileName);
+            byte[] fileByteArray = System.IO.File.ReadAllBytes(fullPath);
+            System.IO.File.Delete(fullPath);    // detele the excel file
+            return File(fileByteArray, "application/vnd.ms-excel", fileName);
+        }
     }
 }
