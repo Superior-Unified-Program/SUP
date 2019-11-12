2b90311a02cb5812ff08bc02f785fd96abd64822
epyon36959 <kellen937058391@gmail.com>
2019-11-11 21:28:10 -0600

    add adduser page, more detail need to change

---
+++ SUP/SUP-MVC/Models/AddUser/AddUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SUP_MVC.Models.AddUser
+{
+    public class AddUserViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
