51ffff45a0c2bd321714373227216492d4a1ac4b
epyon36959 <kellen937058391@gmail.com>
2019-11-13 08:50:17 -0600

    finish MVC for AddUser

--- SUP/SUP-MVC/Controllers/AddUserController.cs
+++ SUP/SUP-MVC/Controllers/AddUserController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SUP_Library;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,90 @@ namespace SUP_MVC.Controllers
 {
     public class AddUserController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
+
+        // GET: AddUser/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AddUser/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AddUser/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AddUser/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AddUser/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AddUser/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AddUser/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        
     }
 }
