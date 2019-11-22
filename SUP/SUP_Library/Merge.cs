dace4d5cb2f2c295c5f471b05521ea9fb0dcfbbd
Marcus Parmeter <mparmet3@uwsuper.edu>
2019-11-22 08:45:02 -0600

    Added more check boxes for new database fields.

--- SUP/SUP_Library/Merge.cs
+++ SUP/SUP_Library/Merge.cs
@@ -24,13 +24,13 @@ namespace SUP_Library
     /// </summary>
     public class Merge
     {
-        //private static string templatePath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\Templates");
-        //private static string savePath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\Temp");
-        //private static string zipPath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\ExportFileFolder");
+		//private static string templatePath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\Templates");
+		//private static string savePath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\Temp");
+		//private static string zipPath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\ExportFileFolder");
 
-		private static string templatePath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\Templates");
-		private static string savePath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\Temp");
-		private static string zipPath = (Directory.GetCurrentDirectory()).Replace("SUP-MVC", "SUP_Library\\ExportFileFolder");
+		private static string templatePath = @"C:\Users\Public\Documents\Templates";
+		private static string savePath = @"C:\Users\Public\Documents\Temp";
+		private static string zipPath = @"C:\Users\Public\Documents\ExportFileFolder";
 		private struct token
         {
             public string open;
